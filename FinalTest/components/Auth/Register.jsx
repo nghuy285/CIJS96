925ff103148e4d2bdcaa64857da5138353ae2a3f
nghuy285 <huyngo28052005@gmail.com>
2024-07-17 19:40:26 +0700

    update

--- FinalTest/components/Auth/Register.jsx
+++ FinalTest/components/Auth/Register.jsx
@@ -46,6 +46,31 @@ const Register = () => {
           >
             <Input.Password placeholder="Password" />
           </Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error(
+                      "The two passwords that you entered do not match!"
+                    )
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="Confirm Password" />
+          </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit" block>
               Register
